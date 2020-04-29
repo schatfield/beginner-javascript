93285fc0778ab2d261549cb640127c3fb4043540
Schatfield <dgoldmine@icloud.com>
2020-04-29 17:08:56 -0500

    creating HTML in the dom

--- exercises/20 - The DOM/creating-FINISHED.js
+++ exercises/20 - The DOM/creating-FINISHED.js
@@ -1,56 +1,79 @@
+/* eslint-disable */
 console.log('it works!');
 
 const myParagraph = document.createElement('p');
-myParagraph.textContent = 'I am a P';
-myParagraph.classList.add('special');
+myParagraph.textContent ='i am a P';
+myParagraph.classList.add('SPECIAL');
 console.log(myParagraph);
 
 const myImage = document.createElement('img');
-myImage.src = 'https://picsum.photos/500';
+myImage.src = 'https://picsum.photos/600';
 myImage.alt = 'Nice photo';
+console.log(myImage);
 
 const myDiv = document.createElement('div');
 myDiv.classList.add('wrapper');
 console.log(myDiv);
 
+// appendChild() addsa node- a node can be either an element or a plain piece of text
+
+document.body.appendChild(myDiv);
 myDiv.appendChild(myParagraph);
 myDiv.appendChild(myImage);
 
-document.body.appendChild(myDiv);
+// console.log('it works!');
 
-// oh shoot! we need to add somethint to the top. like a heading!
-const heading = document.createElement('h2');
-heading.textContent = 'Cool Things';
+// const myParagraph = document.createElement('p');
+// myParagraph.textContent = 'I am a P';
+// myParagraph.classList.add('special');
+// console.log(myParagraph);
 
-myDiv.insertAdjacentElement('beforebegin', heading);
+// const myImage = document.createElement('img');
+// myImage.src = 'https://picsum.photos/500';
+// myImage.alt = 'Nice photo';
 
-// <ul>
-// <li>One</li>
-// <li>two</li>
-// <li>three</li>
-// <li>four</li>
-// <li>five</li>
-// </ul>
+// const myDiv = document.createElement('div');
+// myDiv.classList.add('wrapper');
+// console.log(myDiv);
 
-const list = document.createElement('ul');
-const li = document.createElement('li');
-li.textContent = 'three';
-list.appendChild(li);
+// myDiv.appendChild(myParagraph);
+// myDiv.appendChild(myImage);
 
-document.body.insertAdjacentElement('afterbegin', list);
+// document.body.appendChild(myDiv);
 
-const li5 = document.createElement('li');
-li5.textContent = 'Five';
-list.append(li5);
+// // oh shoot! we need to add somethint to the top. like a heading!
+// const heading = document.createElement('h2');
+// heading.textContent = 'Cool Things';
 
-const li1 = li5.cloneNode(true);
-li1.textContent = 'one';
-list.insertAdjacentElement('afterbegin', li1);
+// myDiv.insertAdjacentElement('beforebegin', heading);
 
-const li4 = document.createElement('li');
-li4.textContent = 'four';
-li5.insertAdjacentElement('beforebegin', li4);
+// // <ul>
+// // <li>One</li>
+// // <li>two</li>
+// // <li>three</li>
+// // <li>four</li>
+// // <li>five</li>
+// // </ul>
 
-const li2 = document.createElement('li');
-li2.textContent = 'two';
-li1.insertAdjacentElement('afterend', li2);
+// const list = document.createElement('ul');
+// const li = document.createElement('li');
+// li.textContent = 'three';
+// list.appendChild(li);
+
+// document.body.insertAdjacentElement('afterbegin', list);
+
+// const li5 = document.createElement('li');
+// li5.textContent = 'Five';
+// list.append(li5);
+
+// const li1 = li5.cloneNode(true);
+// li1.textContent = 'one';
+// list.insertAdjacentElement('afterbegin', li1);
+
+// const li4 = document.createElement('li');
+// li4.textContent = 'four';
+// li5.insertAdjacentElement('beforebegin', li4);
+
+// const li2 = document.createElement('li');
+// li2.textContent = 'two';
+// li1.insertAdjacentElement('afterend', li2);
