9a211763c9dd0a8b0ceeae54c9fb38e52561fa08
Schatfield <dgoldmine@icloud.com>
2020-04-30 17:31:38 -0500

    creating and dumping elements into the DOM with .innerHTML

--- exercises/20 - The DOM/creating-with-strings-FINISHED.js
+++ exercises/20 - The DOM/creating-with-strings-FINISHED.js
@@ -1,16 +1,69 @@
+/* eslint-disable */
+// console.log('it working')
+// variable storing first element with a class of item that is found on the page
 const item = document.querySelector('.item');
 
+// inserts the h1 tag into the class of item 
+// backticks let you do multi line html
+// setting innerhtml into your element is one way to just dump a string in and have the browser create all the elements for you
+// backticks interpolate values very easily
 const width = 500;
 const src = `https://picsum.photos/${width}`;
-const desc = `Cute Pup <img onload="alert('HACKED')" src="https://picsum.photos/50"/>`;
+const desc = `Cute Pup`
 const myHTML = `
-  <div class="wrapper">
-    <h2>Cute ${desc}</h2>
-    <img src="${src}" alt="${desc}"/>
-  </div>
+<div class="wrapper">
+  <h2>Cute pup</h2>
+  <img src="${src}" alt="${desc}"/>
+</div>
 `;
 
-// turn a string into a DOM element
-const myFragment = document.createRange().createContextualFragment(myHTML);
 
-document.body.appendChild(myFragment);
+
+// this below will give you an error because myHTML is not an element it is only a string. 
+// myHTML.classList.add('special');
+// this consolelog shows that myHTML is a string
+// console.log(typeof myHTML);
+
+// // this will come up as undefined because myHTML is just a string and not an element
+// // it only becomes an element once it is dumped into the DOM by setting the innerHtml
+// console.log(myHTML.classList);
+
+// // dumping myHTML into the DOM by setting the innerHTML. 
+// // item is the variable from the beginning that is holding the reference to the 1st element with the class .item
+// item.innerHTML = myHTML;
+
+// const itemImage = document.querySelector('.wrapper img');
+
+// itemImage.classList.add('round');
+
+// console.log(itemImage);
+
+// turn my string into an HTML element
+const myFragment = document.createRange().createContextualFragment(myHTML);
+document.body.append(myFragment)
+
+// myHTML- IS THE RAW STRING
+// myFragment - the fragment that turns the raw string into a fragment that can be dumped into the DOM as an HTML element
+
+// IMPORTANT-
+// if you want to create html from a string you can either dump it straight into an element with .innerHtml
+//OR  if you do need to add event listeners or change classes add classes change attributes you can turn into DOM nodes before it's dumped in with createContextualFragment and pass it a string.
+
+
+
+// const item = document.querySelector('.item');
+
+// const width = 500;
+// const src = `https://picsum.photos/${width}`;
+// const desc = `Cute Pup <img onload="alert('HACKED')" src="https://picsum.photos/50"/>`;
+// const myHTML = `
+//   <div class="wrapper">
+//     <h2>Cute ${desc}</h2>
+//     <img src="${src}" alt="${desc}"/>
+//   </div>
+// `;
+
+// // turn a string into a DOM element
+// const myFragment = document.createRange().createContextualFragment(myHTML);
+
+// document.body.appendChild(myFragment);
