1565fb489c3bc3b0d5c4a577767a91424967c566
Schatfield <dgoldmine@icloud.com>
2020-05-01 19:38:29 -0500

    remove method exercise

--- exercises/20 - The DOM/traversing.js
+++ exercises/20 - The DOM/traversing.js
@@ -10,3 +10,10 @@ console.log(shawna.lastElementChild);
 console.log(shawna.previousElementSibling);
 console.log(shawna.nextElementSibling);
 console.log(shawna.parentElement);
+
+const p = document.createElement('p');
+p.textContent = 'I will be removed';
+document.body.appendChild(p);
+
+p.remove();
+console.log(p);
