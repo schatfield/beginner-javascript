ae0e93b34305bd97847c80f7fc6564b49941b2a6
Schatfield <dgoldmine@icloud.com>
2020-05-06 17:21:33 -0500

    added randomizing functionality for starting point of drawing on canvas, created handler function for arrow key event keydown, created event listener for keydown event.

--- exercises/33 - Etch-a-Sketch/etch-a-sketch.js
+++ exercises/33 - Etch-a-Sketch/etch-a-sketch.js
@@ -8,14 +8,43 @@ const shakebutton = document.querySelector('.shake');
 
 // set up our canvas for drawing
 
+// if you are making variable form property on an object you can shortform write this below like this:
+// this format is called destructuring 
+// make a variable called height, make a variable called width from the matching properties on our canvas
+// const width = canvas.width;
+// const height = canvas.height;
+
+const  { width, height } = canvas;
+
+// create random x and y  starting points on the canvas
+// we will use Math.floor() and Math.random()
+// Math.floor - run Math.floor against Math.random it will give you the lower level of the random decimal returned by Math.random()
+// Math.random only gives random numbers that are under zero or zero, takes no args. if you do Math.random() * 100 you'll get a random number between 0 and 99.999999
+// replace your 200 px number in moveTo and lineTo belwo to use variables x and y
+let x = Math.floor(Math.random() * width);
+let y = Math.floor(Math.random() * width);
+
+
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
-ctx.Width = 10;
+ctx.lineWidth = 10;
+
+ctx.beginPath(); //this is how you start the drawing- it's like putting a marker to a page
+ctx.moveTo(x, y); // 200 px from the top 200px down
+ctx.lineTo(x, y); //moveTo and lineTo create an invisible line
+ctx.stroke(); //this will draw an actual line from where you started to where you ended
 
 // write a draw function
 
 // write a handler for the keys
 
+function handleKey(e) {
+    e.preventDefault()
+    console.log(e.key);
+    console.log('handle key');
+}
+
 // clear/shake function
 
 // listen for arrow keys
+window.addEventListener('keydown', handleKey);
