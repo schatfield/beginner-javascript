a349d6cee3007ccb53e4da18006a7f008e00cd1b
Schatfield <dgoldmine@icloud.com>
2020-05-08 18:13:24 -0500

    completed handleclick function which populates the modal with content when the button on each card is clicked thus showing the modal

--- exercises/34 - Click Outside/click-outside.js
+++ exercises/34 - Click Outside/click-outside.js
@@ -7,23 +7,27 @@ const cardButtons = document.querySelectorAll('.card button');
 
 // grab the modal. do it here becaue we ony need to select it once, if we needed to selectit with every click we would put it in the handlecardbuttonclick function
 const modalInner = document.querySelector('.modal-inner');
+const modalOuter = document.querySelector('.modal-outer');
+
+// This function below is going to grab something to show in the modal when the 'learn more' button in each card is clicked- the image and the data description of the parent card will show up in the modal
 
 function handleCardButtonClick(event) {
   console.log('ya clicked it');
   const button = event.currentTarget;
-  // grabs the individual card for the button you clicked on. the closest thing with a class of .card to the button clicked
-  // .closest() is similar to queryselectorall but opposite- it climnbs up the DOM tree of elements instead down
+  // find the card that is associated with the clicked button
+  // .closest() is similar to queryselectorall but opposite- it climbs up the DOM tree of elements instead down
   const card = button.closest('.card');
-  // shows you the card associated with the button clicked
-  console.log(card);
   // grab the image src
   const imgSrc = card.querySelector('img').src;
-  console.log(imgSrc);
   //  grab the data desc of the parent card. if we want to grab a data-desciption from the dom we use .dataset
   const desc = card.dataset.description;
-  // shows you the description you hust grabbed in const desc
-  console.log(desc);
+  const name = card.querySelector('h2').textContent;
   // populate the modal with the new info. grab the image and show higher version of it
+  modalInner.innerHTML = `
+  <img src="${imgSrc.replace('200', '600')}" alt="${name}"/>
+  <p>${desc}</p>`;
+  // show the modal
+  modalOuter.classList.add('open');
 }
 
 // loop over each of the buttons and listen for a click on them
@@ -31,6 +35,21 @@ cardButtons.forEach(button =>
   button.addEventListener('click', 
   handleCardButtonClick)
   );
+
+function closeModal() {
+  modalOuter.classList.remove('open');
+}
+
+modalOuter.addEventListener('click', function(event) {
+  // .closest() is similar to queryselectorall but opposite- it climbs up the DOM tree of elements instead down
+  const isOutside = !event.target.closest('.modal-inner');
+  if (isOutside) {
+    modalOuter.classList.remove('open');
+  }
+  // console.log(event.target);
+  // console.log(event.currentTarget);
+  console.log(isOutside);
+})
 
 // const cardButtons = document.querySelectorAll('.card button');
 // const modalOuter = document.querySelector('.modal-outer');
