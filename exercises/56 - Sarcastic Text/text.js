76f23efe5ce7aa8049aa2f3088e5e15498cd2532
Schatfield <dgoldmine@icloud.com>
2020-06-24 14:21:11 -0500

    added functionality to transformText function to identify which radio button is selected by the value on the input tag for each one using .find method

--- exercises/56 - Sarcastic Text/text.js
+++ exercises/56 - Sarcastic Text/text.js
@@ -5,21 +5,15 @@ console.log('it works');
 
 const textArea = document.querySelector('[name="text"]');
 const result = document.querySelector('.result');
-const filterInputs = document.querySelector('[name="filter"]');
+//filterInputs is a nodelist so we wrapped it in Array.from to convert it o an array. this iwll be converted once on page load and this variable will be used in the translateText function to grab the inputs and identify which is selected by the user at at given time
+const filterInputs = Array.from(document.querySelector('[name="filter"]'));
 
 console.log(textArea, result, filterInputs);
-
-
-function transformText(text) {
- //take the text and loop over each letter
-    const mod = Array.from(text).map(filters.sarcastic);
-    console.log(mod);
-    result.textContent = mod.join('');
-}
 
 // an object holding functions (methods) to create the text modifications
 //each property is named for the value in the index.html file
 //the transformText function above is going to run these filter methods in the filters object
+
 const filters = {
     //since we're running these methods on a map above, our sarcastic method can take in all the arguments a regular map will including current value and the index (map takes in an array)
     sarcastic(letter, index) {
@@ -39,6 +33,22 @@ const filters = {
 
     }
 };
+
+
+
+function transformText(text) {
+    // 2 WAYS: grab the filter and change depending on user selection
+    //every time this runs it's going to qerySelector for the thing that's selected. the .value gives the value in the input tag
+    //const filter = document.querySelector('[name="filter"]:checked').value;
+    const filter = filterInputs.firstElementChild(input => input.checked).value;
+    console.log(filter);
+ //take the text and loop over each letter
+    const mod = Array.from(text).map(filters.sarcastic);
+    console.log(mod);
+    result.textContent = mod.join('');
+}
+
+
 
 //listen for the input event, when that happens grab the event and pass the enteredt text to out transformText function
 //grab the text out of the text area? e.target gives us the actual text box and .value gives us whatever the user has passed in 
