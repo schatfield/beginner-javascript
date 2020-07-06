c097f8b61daa47d7fbc59c7158a16bd03c4f9948
Schatfield <dgoldmine@icloud.com>
2020-07-06 15:11:12 -0500

    2 more selections and notes about how this is all working together

--- exercises/58 - Gallery/gallery.js
+++ exercises/58 - Gallery/gallery.js
@@ -8,11 +8,18 @@ function Gallery(gallery) {
   }
   // select the elements we need
   // since we need to select these things for each of our instances, we are selecting the things we need inside this function instead of outside of it. this will scope the variables to the function- they live only inside this function that way gallery1 and 2 will never clash with each other
+
+  //using gallery instead of document here bc we need to scope everything to the actual gallery that was passed into the querySelector below in gallery1 and gallery2 variables
   const images = Array.from(gallery.querySelectorAll('img'));
- 
+  //we are using docmunet here becasue the heml markup for the modal will be shared bewteen the 2 galleries bc only one modal can be open at a time
+  const modal = document.querySelector('.modal');
+  //slecting things inside the modal, using the modal variable from just above
+  const prevButton = modal.querySelector('.prev');
+  const nextButton = modal.querySelector('.next');
 }
 
 //Use it on the page
+//the selectors are being passed in as arguments to the gallery param in the Gallery function above
 
 const gallery1 = Gallery(document.querySelector('.gallery1'));
 const gallery2 = Gallery(document.querySelector('.gallery2'));
