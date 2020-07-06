96f1bdd41c0db356c22e10055150a4ad514ffc38
Schatfield <dgoldmine@icloud.com>
2020-07-06 14:57:55 -0500

    selecting items inside the Gallery closure function

--- exercises/58 - Gallery/gallery.js
+++ exercises/58 - Gallery/gallery.js
@@ -1,4 +1,18 @@
 /*eslint-disable*/
+console.log('it works');
+//a Closure- the ability create a function and creates scope for each of the galleries and have reuseable code
+function Gallery(gallery) {
+  console.log(gallery);
+  if (!gallery) {
+    throw new Error('No Gallery Found');
+  }
+  // select the elements we need
+  // since we need to select these things for each of our instances, we are selecting the things we need inside this function instead of outside of it. this will scope the variables to the function- they live only inside this function that way gallery1 and 2 will never clash with each other
+  const images = Array.from(gallery.querySelectorAll('img'));
+ 
+}
 
-//a Closure- the ability create a function and that function has scope
-function()
+//Use it on the page
+
+const gallery1 = Gallery(document.querySelector('.gallery1'));
+const gallery2 = Gallery(document.querySelector('.gallery2'));
