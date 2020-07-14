90b2177187d7f06d8812855c1b1bbf11c1b5dd95
Schatfield <dgoldmine@icloud.com>
2020-07-14 16:06:59 -0500

    set up function to act as plugin

--- exercises/59 - Slider/src/index.js
+++ exercises/59 - Slider/src/index.js
@@ -1,7 +1,12 @@
 /*eslint-disable*/
+//this will take in a slider div element
+//first thing we want to do is check if somebody has passed in a slider
 function Slider(slider) {
-
+  if (!slider) {
+    throw new Error('No slider passed in');
+  }
 }
+
 
 const mySlider = Slider(document.querySelector('.slider'));
 const dogSlider = Slider(document.querySelector('.dog-slider'));
